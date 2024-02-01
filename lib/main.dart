398ea5ca1caa6ed326df5c774f9214ae9fc7901d
ShayanZZ <shayan_z_71@yahoo.com>
2024-02-02 00:43:34 +0330

    Improve layout adaptability

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:cached_network_image/cached_network_image.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'LoLoPedia',
       theme: ThemeData(
-        primarySwatch: Colors.blue,  
+        primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(),
     );
@@ -24,12 +24,12 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
-  _MyHomePageState createState() => _MyHomePageState(); 
+  _MyHomePageState createState() => _MyHomePageState();
 }
-class _MyHomePageState extends State<MyHomePage> {
 
+class _MyHomePageState extends State<MyHomePage> {
   List champs = [];
-  
+
   String filter = "";
 
   @override
@@ -39,159 +39,168 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> getChamps() async {
-    final response = 
-        await http.get(Uri.parse('https://ddragon.leagueoflegends.com/cdn/14.2.1/data/en_US/champion.json'));
+    final response = await http.get(Uri.parse(
+        'https://ddragon.leagueoflegends.com/cdn/14.2.1/data/en_US/champion.json'));
 
     if (response.statusCode == 200) {
       setState(() {
-        champs = jsonDecode(response.body)['data'].keys.toList();  
+        champs = jsonDecode(response.body)['data'].keys.toList();
       });
     }
   }
 
-    @override 
+  @override
   Widget build(BuildContext context) {
-
-    List filteredChamps = champs.where((champ) => 
-      champ.toLowerCase().contains(filter.toLowerCase())).toList();
-
+    List filteredChamps = champs
+        .where((champ) => champ.toLowerCase().contains(filter.toLowerCase()))
+        .toList();
     return Scaffold(
       appBar: AppBar(
         title: const Text('LoLoPedia'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: TextFormField(
-              onChanged: (value) {
-                setState(() {
-                  filter = value; 
-                });
-              },
-            ),
-          ),
-                    Expanded(
-            child: ListView.builder(
-              itemCount: filteredChamps.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: CachedNetworkImage(
-                    imageUrl: "http://ddragon.leagueoflegends.com/cdn/14.2.1/img/champion/${filteredChamps[index]}.png",
-                    placeholder: (context, url) => const CircularProgressIndicator(),
-                    errorWidget: (context, url, error) => const Icon(Icons.error),
-                  ),
-                  title: Text(filteredChamps[index]),
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(
-                      builder: (context) => DetailPage(champ: filteredChamps[index])));
+      body: Center(
+        child: SizedBox(
+          width: ScreenUtil.calculateMaxWidth(context),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: TextFormField(
+                  onChanged: (value) {
+                    setState(() {
+                      filter = value;
+                    });
                   },
-                );
-              }  
-            ),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: filteredChamps.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: CachedNetworkImage(
+                          imageUrl:
+                              "http://ddragon.leagueoflegends.com/cdn/14.2.1/img/champion/${filteredChamps[index]}.png",
+                          placeholder: (context, url) =>
+                              const CircularProgressIndicator(),
+                          errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
+                        ),
+                        title: Text(filteredChamps[index]),
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => DetailPage(
+                                      champ: filteredChamps[index])));
+                        },
+                      );
+                    }),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
 }
 
 class DetailPage extends StatelessWidget {
-
-  final String champ; 
+  final String champ;
 
   const DetailPage({super.key, required this.champ});
 
-    Future<Map> getChampDetails() async {
-    final response = await http.get(Uri.parse('https://ddragon.leagueoflegends.com/cdn/14.2.1/data/en_US/champion/$champ.json'));
-    return jsonDecode(response.body)['data'][champ]; 
+  Future<Map> getChampDetails() async {
+    final response = await http.get(Uri.parse(
+        'https://ddragon.leagueoflegends.com/cdn/14.2.1/data/en_US/champion/$champ.json'));
+    return jsonDecode(response.body)['data'][champ];
   }
 
-    @override
+  @override
   Widget build(BuildContext context) {
+    double maxWidth = ScreenUtil.calculateMaxWidth(context);
     return Scaffold(
-      body: FutureBuilder(
-        future: getChampDetails(),  
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
+      body: ListView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        children: [
+          Center(
+            child: SizedBox(
+              width: maxWidth,
+              child: FutureBuilder<Map>(
+                future: getChampDetails(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    Map data = snapshot.data as Map;
+                    return Column(
+                      children: [
+                        // Image scrolls
+                        ClipRect(
+                            child: AspectRatio(
+                          aspectRatio: 3 / 2,
+                          child: Image.network(
+                              "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champ}_0.jpg",
+                              height: 300,
+                              width: maxWidth,
+                              fit: BoxFit.cover),
+                        )),
 
-            Map data = snapshot.data as Map;
+                        // Title & Tags underneath image
+                        Container(
+                            padding: const EdgeInsets.all(10),
+                            child: Column(children: [
+                              Text(data['name'],
+                                  style: const TextStyle(
+                                      fontSize: 48,
+                                      fontWeight: FontWeight.bold)),
+                              Wrap(
+                                spacing: 10,
+                                children: data['tags'].map<Widget>((tag) {
+                                  return Chip(label: Text(tag));
+                                }).toList(),
+                              )
+                            ])),
 
-            return ListView(
-              children: [
+                        // Lore
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 10),
+                          child: Text(data['lore'],
+                              style: const TextStyle(fontSize: 18)),
+                        ),
 
-                // Image scrolls  
-                Image.network(
-                  "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champ}_0.jpg",
-                  height: 300,
-                  width: double.infinity,
-                  fit: BoxFit.cover
-                ),
+                        const SizedBox(height: 20),
 
-                // Title & Tags underneath image
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: Column(
-                    children: [
-                      Text(data['name'],
-                        style: const TextStyle(
-                          fontSize: 48,
-                          fontWeight: FontWeight.bold  
-                        )
-                      ),
-                      Wrap(
-                        spacing: 8,
-                        children: data['tags'].map<Widget>((tag) {
-                          return Chip(label: Text(tag));
-                        }).toList(),
-                      )  
-                    ] 
-                  )
-                ),
-
-                // Lore
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 8),
-                  child: Text(data['lore'], style: const TextStyle(fontSize: 18)),
-                ),
-              
-                const SizedBox(height: 40),
-
-                // Spells
-                SpellSection(
-                 spells: data['spells'],
-                 passive: data['passive']
-                )
-              ],
-            );
-
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },  
+                        // Spells
+                        SpellSection(
+                            spells: data['spells'], passive: data['passive'])
+                      ],
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
 }
 
-
 class SpellSection extends StatelessWidget {
-
   final List spells;
   final Map passive;
 
-  const SpellSection({super.key,
-    required this.spells,
-    required this.passive 
-  });
+  const SpellSection({super.key, required this.spells, required this.passive});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-
         ListTile(
           leading: Image.network(
             "https://ddragon.leagueoflegends.com/cdn/14.2.1/img/passive/${passive['image']['full']}",
@@ -203,7 +212,6 @@ class SpellSection extends StatelessWidget {
           title: Text("(P) ${passive['name']}"),
           subtitle: Text(passive['description']),
         ),
-
         ...spells.map((spell) {
           int spellIndex = spells.indexOf(spell);
           String spellKey = "";
@@ -230,15 +238,25 @@ class SpellSection extends StatelessWidget {
                 return const CircularProgressIndicator();
               },
             ),
-            title: Text(
-              "(${spellKey}) ${spell['name']}"
-            ),
+            title: Text("($spellKey) ${spell['name']}"),
             subtitle: Text(spell['description']),
           );
         })
-
       ],
     );
   }
+}
 
+class ScreenUtil {
+  static double calculateMaxWidth(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    bool isFullScreen = MediaQuery.of(context).viewInsets.bottom == 0.0;
+
+    double maxWidth = screenSize.width;
+    if (isFullScreen) {
+      maxWidth = 800.0; // Adjust this value to your desired width
+    }
+
+    return maxWidth;
+  }
 }
